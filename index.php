142444d6498889fc03c194e4b2cc067b6dcc78ba
Yohann <yohann.t@codeur.online>
2017-11-24 20:13:02 +0100

    html/css en cours

--- index.php
+++ index.php
@@ -14,14 +14,14 @@
 
 					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="logos">
 						<img src="assets/img/header/logo_orange.png" alt="logo Orange" width="150" height="150" />
-						<h1>PRES FIMU</h1>
+						<h1 class="text-center">FIMU-Review</h1>
 						<img src="assets/img/header/logo_fimu.png" alt="logo FIMU" width="auto" height="150" />
 					</div>
 				</div>
 			</div>
 
 			<div class="row">
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="no-padding">
 							<nav class="navbar navbar-default">
 								<div class="container">
 									<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
@@ -55,10 +55,152 @@
 			<div class="container">
 				<div class="row text-center">
 					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<h2>Fréquentation globale</h2>
+					</div>
+					<div class="flex-div col-xs-6 col-sm-6 col-md-6 col-lg-6">
+						<div id="graphique">
+							<h3>GRAPHIQUE 2016</h3>
+						</div>
+						<div class="flex-btn btn-group" data-toggle="buttons">
+							<label class="btn btn-primary active">
+							<input type="radio" name="options" id="option1" autocomplete="off" checked> Semaine précédente
+							</label>
+							<label class="btn btn-primary">
+							<input type="radio" name="options" id="option2" autocomplete="off"> Pendant le FIMU
+							</label>
+							<label class="btn btn-primary">
+							<input type="radio" name="options" id="option3" autocomplete="off"> Semaine suivante
+							</label>
+						</div>
+						<div class="flex-btn btn-group" data-toggle="buttons">
+							<label class="btn btn-primary active">
+							<input type="radio" name="options" id="option1" autocomplete="off" checked> CSP
+							</label>
+							<label class="btn btn-primary">
+							<input type="radio" name="options" id="option2" autocomplete="off"> Âge
+							</label>
+							<label class="btn btn-primary">
+							<input type="radio" name="options" id="option3" autocomplete="off"> Origine
+							</label>
+						</div>
+					</div>
+						
+					
+					<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+						<h3>GRAPHIQUE 2017</h3>
+						<div class="btn-group" data-toggle="buttons">
+							<label class="btn btn-primary active">
+							<input type="radio" name="options" id="option1" autocomplete="off" checked> Radio 1
+							</label>
+							<label class="btn btn-primary">
+							<input type="radio" name="options" id="option2" autocomplete="off"> Radio 2
+							</label>
+							<label class="btn btn-primary">
+							<input type="radio" name="options" id="option3" autocomplete="off"> Radio 3
+							</label>
+						</div>
+						
+					</div >
+					</div>
+					<div class="para col-xs-12 col-sm-6 col-md-3 col-lg-3">
+						<p>25%</p>
+						<p>de participation</p>
+					</div>
+					<div class="para col-xs-12 col-sm-6 col-md-3 col-lg-3">
+						<p>25%</p>
+						<p>de participation</p>
+					</div>
+					<div class="para col-xs-12 col-sm-6 col-md-3 col-lg-3">
+						<p>25%</p>
+						<p>de participation</p>
+					</div>
+					<div class="para col-xs-12 col-sm-6 col-md-3 col-lg-3">
+						<p>25%</p>
+						<p>de participation</p>
+					</div>
+				
+			</div>
+	   </div>
+	   
+	   <!-- /////////////////////////////////////////////////////////-->
+	   
+	   <div class="container-fluid" id="bloc2">
+			<div class="container">
+				<div class="row text-center">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 						<h2>CATEGORIE</h2>
 					</div>
 					<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
 						<h3>GRAPHIQUE 2016</h3>
+						<div class="btn-group" data-toggle="buttons">
+							<label class="btn btn-primary active">
+							<input type="radio" name="options" id="option1" autocomplete="off" checked> Radio 1
+							</label>
+							<label class="btn btn-primary">
+							<input type="radio" name="options" id="option2" autocomplete="off"> Radio 2
+							</label>
+							<label class="btn btn-primary">
+							<input type="radio" name="options" id="option3" autocomplete="off"> Radio 3
+							</label>
+						</div>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+						<h3>GRAPHIQUE 2017</h3>
+						<div class="btn-group" data-toggle="buttons">
+							<label class="btn btn-primary active">
+							<input type="radio" name="options" id="option1" autocomplete="off" checked> Radio 1
+							</label>
+							<label class="btn btn-primary">
+							<input type="radio" name="options" id="option2" autocomplete="off"> Radio 2
+							</label>
+							<label class="btn btn-primary">
+							<input type="radio" name="options" id="option3" autocomplete="off"> Radio 3
+							</label>
+						</div>
+						
+					</div >
+					<div class="para col-xs-12 col-sm-6 col-md-3 col-lg-3">
+						<p>25%</p>
+						<p>de participation</p>
+					</div>
+					<div class="para col-xs-12 col-sm-6 col-md-3 col-lg-3">
+						<p>25%</p>
+						<p>de participation</p>
+					</div>
+					<div class="para col-xs-12 col-sm-6 col-md-3 col-lg-3">
+						<p>25%</p>
+						<p>de participation</p>
+					</div>
+					<div class="para col-xs-12 col-sm-6 col-md-3 col-lg-3">
+						<p>25%</p>
+						<p>de participation</p>
+					</div>
+				</div>
+			</div>
+	   </div>
+	   
+	   <!-- /////////////////////////////////////////////////////////-->
+	   
+	   <div class="container-fluid" id="bloc3">
+			<div class="container">
+				<div class="row text-center">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<h2>CATEGORIE</h2>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+						<h3>GRAPHIQUE 2016</h3>
+						
+						<div class="btn-group" data-toggle="buttons">
+							<label class="btn btn-primary active">
+							<input type="radio" name="options" id="option1" autocomplete="off" checked> Radio 1
+							</label>
+							<label class="btn btn-primary">
+							<input type="radio" name="options" id="option2" autocomplete="off"> Radio 2
+							</label>
+							<label class="btn btn-primary">
+							<input type="radio" name="options" id="option3" autocomplete="off"> Radio 3
+							</label>
+						</div>
 						<div class="btn-group" data-toggle="buttons">
 							<label class="btn btn-primary active">
 							<input type="radio" name="options" id="option1" autocomplete="off" checked> Radio 1
