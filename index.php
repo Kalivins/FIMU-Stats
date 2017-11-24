1bfc81f3120f41c5d2e41c989f53411e87a39637
Yohann <yohann.t@codeur.online>
2017-11-24 14:39:53 +0100

    debut html/css

--- index.php
+++ index.php
@@ -7,8 +7,111 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    
-    
+   <header>   		
+		<div class="container-fluid" id="header">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="logos">
+						<img src="assets/img/header/logo_orange.png" alt="logo Orange" width="150" height="150" />
+						<h1>PRES FIMU</h1>
+						<img src="assets/img/header/logo_fimu.png" alt="logo FIMU" width="auto" height="150" />
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+							<nav class="navbar navbar-default">
+								<div class="container">
+									<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+										<div class="navbar-header">
+											<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+												 <span class="icon-bar"></span>
+												 <span class="icon-bar"></span>
+												 <span class="icon-bar"></span>
+											</button>
+										</div>
+
+										<div class="collapse navbar-collapse" id="myNavbar">
+											<ul class="nav navbar-nav">
+												<li><a href="#qui">Qui suis-je?</a></li>
+												<li><a href="#formation">Formations</a></li>
+												<li><a href="#competences">Compétences</a></li>
+												<li><a href="#experience">Expériences</a></li>
+											</ul>
+										</div>
+
+									</div>
+								</div>
+							</nav>
+					</div>
+				</div>
+		</div>
+</header>
+   <main>
+   		<div class="container-fluid" id="bloc1">
+			<div class="container">
+				<div class="row text-center">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<h2>CATEGORIE</h2>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+						<h3>GRAPHIQUE 2016</h3>
+						<div class="btn-group" data-toggle="buttons">
+							<label class="btn btn-primary active">
+							<input type="radio" name="options" id="option1" autocomplete="off" checked> Radio 1
+							</label>
+							<label class="btn btn-primary">
+							<input type="radio" name="options" id="option2" autocomplete="off"> Radio 2
+							</label>
+							<label class="btn btn-primary">
+							<input type="radio" name="options" id="option3" autocomplete="off"> Radio 3
+							</label>
+						</div>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+						<h3>GRAPHIQUE 2017</h3>
+						<div class="btn-group" data-toggle="buttons">
+							<label class="btn btn-primary active">
+							<input type="radio" name="options" id="option1" autocomplete="off" checked> Radio 1
+							</label>
+							<label class="btn btn-primary">
+							<input type="radio" name="options" id="option2" autocomplete="off"> Radio 2
+							</label>
+							<label class="btn btn-primary">
+							<input type="radio" name="options" id="option3" autocomplete="off"> Radio 3
+							</label>
+						</div>
+						
+					</div >
+					<div class="para col-xs-12 col-sm-6 col-md-3 col-lg-3">
+						<p>25%</p>
+						<p>de participation</p>
+					</div>
+					<div class="para col-xs-12 col-sm-6 col-md-3 col-lg-3">
+						<p>25%</p>
+						<p>de participation</p>
+					</div>
+					<div class="para col-xs-12 col-sm-6 col-md-3 col-lg-3">
+						<p>25%</p>
+						<p>de participation</p>
+					</div>
+					<div class="para col-xs-12 col-sm-6 col-md-3 col-lg-3">
+						<p>25%</p>
+						<p>de participation</p>
+					</div>
+				</div>
+			</div>
+	   </div>
+		
+
+   	
+   </main> 
+   <footer>
+   	
+   </footer> 
     
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/app.js"></script>
